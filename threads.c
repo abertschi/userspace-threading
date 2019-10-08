b7c90fd5c02428d43c6e5d45c9bf17e929d14c6f
Andrin Bertschi <abertschi@abertschi.ch>
2019-10-08 16:07:53 +0200

    add cmake support

---
+++ threads.c
@@ -0,0 +1,44 @@
+#include "threads.h"
+
+struct thread *
+thread_create(void (*f)(void *), void *arg)
+{
+
+}
+
+void
+thread_add_runqueue(struct thread *t)
+{
+
+}
+
+void
+thread_yield(void)
+{
+
+}
+
+void
+dispatch(void)
+{
+
+}
+
+void
+schedule(void)
+{
+
+}
+
+void
+thread_exit(void)
+{
+
+}
+
+void
+thread_start_threading(void)
+{
+
+}
+
